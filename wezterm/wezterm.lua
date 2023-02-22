61961cfd6de1a2cdf8bd22348e0842b8bf8347ed
Daniel Choi <choi.mina@gmail.com>
2023-02-22 14:10:15 -0500

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,27 +2,9 @@ local wezterm = require 'wezterm';
 
 return {
 	force_reverse_video_cursor = true,
-	colors = {
-		foreground = "#dcd7ba",
-		background = "#1f1f28",
-
-		cursor_bg = "#c8c093",
-		cursor_fg = "#c8c093",
-		cursor_border = "#c8c093",
-
-		selection_fg = "#c8c093",
-		selection_bg = "#2d4f67",
-
-		scrollbar_thumb = "#16161d",
-		split = "#16161d",
-
-		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-	},
-	-- color_scheme = "nord",
+	color_scheme = "nord",
 	font = wezterm.font({ family = "Hasklug Nerd Font Mono", weight = "Light", freetype_render_target = "HorizontalLcd", harfbuzz_features = {"calt=0", "clig=0", "liga=0"} }),
-	font_size = 23,
+	font_size = 17,
 	window_close_confirmation = "NeverPrompt",
 	hide_tab_bar_if_only_one_tab = true,
 	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
@@ -47,8 +29,8 @@ return {
 		bottom = 5,
 	},
 	initial_rows = 45,
-	initial_cols = 160,
+	initial_cols = 170,
 	native_macos_fullscreen_mode = true,
 	exit_behavior = "Close",
-	max_fps = 60,
+	max_fps = 120,
 }
