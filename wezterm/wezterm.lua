738e0f9a945b93555b89f23507ad8f2736dd8738
Daniel Choi <choi.mina@gmail.com>
2023-03-20 21:02:03 -0400

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
 	force_reverse_video_cursor = true,
 	color_scheme = "nord",
 	font = wezterm.font({ family = "Hasklug Nerd Font Mono", weight = "Light", freetype_render_target = "HorizontalLcd", harfbuzz_features = {"calt=0", "clig=0", "liga=0"} }),
-	font_size = 17,
+	font_size = 13,
 	window_close_confirmation = "NeverPrompt",
 	hide_tab_bar_if_only_one_tab = true,
 	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
@@ -21,6 +21,9 @@ return {
 		{ key = "RightArrow", mods = "SHIFT|CMD", action = wezterm.action{ ActivatePaneDirection = "Right"} },
 		{ key = "UpArrow", mods = "SHIFT|CMD", action = wezterm.action{ ActivatePaneDirection = "Up"} },
 		{ key = "DownArrow", mods = "SHIFT|CMD", action = wezterm.action{ ActivatePaneDirection= "Down"} },
+		{ key = 'l', mods = 'ALT', action = wezterm.action{ ActivateTabRelative =-1} },
+		{ key = 'h', mods = 'ALT', action = wezterm.action{ ActivateTabRelative = 1} },
+
 	},
 	window_padding = {
 		left = 5,
